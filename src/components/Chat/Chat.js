ac7affa3821cbf7e52cb5cdd0a7283efe31c5610
Cedric Crisolo <cedfi@MacBook-Pro-3.attlocal.net>
2020-05-12 16:57:21 -0700

    make chat app until Input component

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+
+//all socket.io logic will be stored here
+//location returns a URL
+import './Chat.css';
+
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+let socket;
+
+
+const Chat = ({ location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+    const ENDPOINT = 'localhost:5000';
+    //use useEffect hook
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+
+        socket.emit('join', { name, room }, () => {
+
+        });
+        
+        return () => {
+            socket.emit('disconnect');
+
+            socket.off();
+        }
+    }, [ENDPOINT, location.search]);
+
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages, message]);
+        })
+    }, [messages]);
+
+    //function for sending messages
+    const sendMessage = (event) => {
+        event.preventDefault();
+
+        if(message) {
+            socket.emit('sendMessage', message, () => setMessage(''));
+        }
+    }
+    console.log(message, messages);
+
+
+    return (
+        <div className='outerContainer'>
+            <div className='container'>
+                <InfoBar room={room}/>
+
+                <Input message={message} setMessage={setMessage} sendMessage={sendMessage}/>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Chat;
+
+
